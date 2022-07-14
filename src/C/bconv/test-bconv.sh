5f7082cd6dc12078e0fabd173f7013c5a8bdd23a
haru864 <kawachigenji95@gmail.com>
2022-07-15 02:38:45 +0900

    test script for bconv

--- src/C/bconv/test-bconv.sh
+++ src/C/bconv/test-bconv.sh
@@ -9,16 +9,25 @@ COMMAND_NOMINAL=(
 )
 
 RESULE_NOMINAL=(
-    "0 0 0 0"
-    "1011 13 11 c"
-    "111000 70 56 38"
-    "100000 40 32 20"
-    "10101111 257 175 AF"
+    # "0 0 0 0"
+    # "1011 13 11 c"
+    # "111000 70 56 38"
+    # "100000 40 32 20"
+    # "10101111 257 175 AF"
+    "binary: 0 octal: 0 decimal: 0 hexadecimal: 0"
+    "binary: 1011 octal: 13 decimal: 11 hexadecimal: B"
+    "binary: 111000 octal: 70 decimal: 56 hexadecimal: 38"
+    "binary: 100000 octal: 40 decimal: 32 hexadecimal: 20"
+    "binary: 10101111 octal: 257 decimal: 175 hexadecimal: AF"
 )
 
-for i in "${COMMAND_NOMINAL[@]}"; do
-    RESULT=`eval $i`
-    for j in "${RESULT[@]}"; do
-        echo ${j}
-    done
+for i in `seq 0 4`; do
+    RESULT=`eval ${COMMAND_NOMINAL[i]}`
+    # echo ${RESULT}
+    # echo ${RESULE_NOMINAL[i]}
+    if [ "`echo ${RESULT}`" = "${RESULE_NOMINAL[i]}" ]; then
+        echo "CASE${i}: PASS"
+    else
+        echo "CASE${i}: FAILURE"
+    fi
 done
